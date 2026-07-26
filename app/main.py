104948b1139fb87fea9a92bfb33f8aa3b4249869
muzml <muzamilbng@gmail.com>
2026-07-26 12:05:39 +0530

    feat: implement GET /tasks and GET /tasks/{id} endpoints

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+
+from app.schemas import TaskResponse
+from app.database import tasks_db, find_task_by_id
 
 # Initialize the FastAPI application instance
 app = FastAPI(
@@ -32,3 +36,32 @@ def get_health():
     return {
         "status": "ok"
     }
+
+
+@app.get("/tasks", response_model=list[TaskResponse], status_code=status.HTTP_200_OK)
+def get_all_tasks():
+    """
+    Get All Tasks Endpoint (GET /tasks)
+    
+    Returns a list of all tasks stored in memory.
+    If no tasks exist, returns an empty list [].
+    """
+    return tasks_db
+
+
+@app.get("/tasks/{id}", response_model=TaskResponse, status_code=status.HTTP_200_OK)
+def get_single_task(id: int):
+    """
+    Get Single Task Endpoint (GET /tasks/{id})
+    
+    - Accepts 'id' as a path parameter (integer).
+    - Returns the task matching the specified 'id'.
+    - If task is not found, returns HTTP 404 with {"error": "Task not found"}.
+    """
+    task = find_task_by_id(id)
+    if not task:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"error": "Task not found"}
+        )
+    return task
